18c138ccd12807f59f74f8cd3622ed802dc87b87
MateusRamosv2 <MateusRamosv2>
2025-10-23 21:18:23 +0000

    transformando JSON bruto da camada bronze em estrutura tabular e salvando em Parquet

--- silver/02_transformacao_silver.py
+++ silver/02_transformacao_silver.py
@@ -1,2 +1,27 @@
 # Databricks notebook source
+import json
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+
+# Lê dados da camada Bronze
+df_bronze = spark.read.parquet("dbfs:/Volumes/workspace/default/arquivos-projetos/01_ingestao_api_ibge.parquet")
+
+# Extrai o campo JSON
+data = json.loads(df_bronze.collect()[0]["raw_json"])
+
+serie = data[0]['resultados'][0]['series'][0]['serie']
+df_silver = spark.createDataFrame(
+    [(k, float(v)) for k, v in serie.items()],
+    ["periodo", "ipca"]
+)
+
+# Limpa formato do período
+df_silver = df_silver.withColumn("periodo", f.regexp_replace(f.col("periodo"), "M", "/"))
+
+display(df_silver)
+
+# Salva na camada Silver
+df_silver.write.mode("overwrite").parquet(
+    "dbfs:/Volumes/workspace/default/arquivos-projetos/02_transformacao_silver"
+)
 
